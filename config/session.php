d70e47be1a3ae6681b422dafb02e10a73d3ea9b6
usersalsabey <231210058@student.mercubuana-yogya.ac.id>
2026-04-28 11:23:00 +0700

    fix session config

--- config/session.php
+++ config/session.php
@@ -198,8 +198,8 @@ return [
     | Supported: "lax", "strict", "none", null
     |
     */
-
-    'same_site' => env('SESSION_SAME_SITE', 'lax'),
+'secure' => env('SESSION_SECURE_COOKIE', true),
+'same_site' => env('SESSION_SAME_SITE', 'lax'),
 
     /*
     |--------------------------------------------------------------------------
